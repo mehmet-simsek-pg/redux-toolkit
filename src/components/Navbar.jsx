febdfc51717906603b7a6c44c2e50c5c316b8fa4
Mehmet <simsekmehmet00@gmail.com>
2023-03-23 16:15:46 +0100

    clean code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
+import * as React from "react"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import { useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { clearUser } from "../features/authSlice"
 
 export default function Navbar() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  //TODO user bilgisini global state'den oku
+  const { user } = useSelector((state) => state.auth)
+  console.log(user)
+
+  const handleLogout = () => {
+    //TODO user global state'ini sil
+    dispatch(clearUser())
+    navigate("/login")
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -21,9 +33,18 @@ export default function Navbar() {
             Clarusway News
           </Typography>
 
-          <Button color="inherit">Login</Button>
+          {user?.email && user.password && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+          {!user?.email && (
+            <Button color="inherit" onClick={() => navigate("/login")}>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,30 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { useState } from "react";
+import * as React from "react"
+import Button from "@mui/material/Button"
+import TextField from "@mui/material/TextField"
+import Link from "@mui/material/Link"
+import Box from "@mui/material/Box"
+import Avatar from "@mui/material/Avatar"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { setUser } from "../features/authSlice"
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setEmail("");
-    setPassword("");
-  };
+    e.preventDefault()
+    // TODO dispatch( user guncelleyen fonksiyon())
+    dispatch(setUser({ email, password }))
+    setEmail("")
+    setPassword("")
+    navigate("/")
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -36,7 +44,7 @@ export default function Login() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
           <TextField
             margin="normal"
             required
@@ -83,5 +91,5 @@ export default function Login() {
         {"."}
       </Typography>
     </Container>
-  );
+  )
 }
